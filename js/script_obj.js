08a3df4cac01129e2a4951f690e2180ffef932fb
Oleh <example@gmail.com>
2022-09-25 20:54:20 +0200

    New obj lesson

---
+++ js/script_obj.js
@@ -0,0 +1,14 @@
+'use strict'
+
+const person={
+    name: "TestName",
+    surname: "TestSurname",
+    phone: "+3524786445",
+    marks: {
+        tatoo:"dragon",
+        hair: "blue",
+    },
+    book: "best book ever"
+}
+
+console.log(person);
