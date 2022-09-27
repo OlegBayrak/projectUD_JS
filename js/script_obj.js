b27de50578177eda27eef164df77135afa4991da
Oleh <example@gmail.com>
2022-09-27 18:52:26 +0200

    New commit

--- js/script_obj.js
+++ js/script_obj.js
@@ -1,14 +1,47 @@
-'use strict'
+//const { result } = require("cypress/types/lodash");
 
-const person={
-    name: "TestName",
-    surname: "TestSurname",
-    phone: "+3524786445",
-    marks: {
-        tatoo:"dragon",
-        hair: "blue",
-    },
-    book: "best book ever"
-}
+const personalPlanPeter = {
+    name: "Peter",
+    age: "29",
+    skills: {
+        languages: ['ru', 'eng'],
+        programmingLangs: {
+            js: '20%',
+            php: '10%',
+            ruby: '30%'
+        },
+        exp: '1 month',
+        }, 
+        showAgeAndLangs : function(plan_obj) {
+            const {name, age, skills} = personalPlanPeter;
+            const {languages, programmingLangs,exp} = personalPlanPeter.skills;
+            result = `Мне ${age} и я владею языками: `;
+            var langs = "";
+            for (let i = 0; i<= languages.length-1; i++){
+                langs = languages[i];
+                result = result + langs.toUpperCase() + ' ';
+            }
+            return result;
+    }
+};
 
-console.log(person);
+console.log(personalPlanPeter.showAgeAndLangs(personalPlanPeter));
+
+
+function showExperience(plan) {
+    const {languages, programmingLangs, exp} = personalPlanPeter.skills;
+    return exp;
+    }
+
+    function showProgrammingLangs(plan) {
+        let result = "";
+        for (let key in plan.skills.programmingLangs){
+            if (plan.skills.programmingLangs[key] == ''){
+                result = result + `Язык ${key} изучен на undefined` + '\n'; 
+            } else{
+            result = result + `Язык ${key} изучен на ${personalPlanPeter.skills.programmingLangs[key]}` + '\n'; }
+            //console.log(`Язык ${key} изучен на ${personalPlanPeter.skills.programmingLangs[key]}` );
+        }
+        return result;
+    }
+
