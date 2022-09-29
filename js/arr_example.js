e63c48f0c5f38cee5bfc4e62fe8cfd0ac0af634a
Oleh <example@gmail.com>
2022-09-29 14:32:57 +0200

    New OOP topic start

---
+++ js/arr_example.js
@@ -0,0 +1,14 @@
+'use strict'
+
+const someString = 'This is some strange string';
+
+function reverse(str) {
+
+}
+
+const baseCurrencies = ['USD', 'EUR'];
+const additionalCurrencies = ['UAH', 'RUB', 'CNY'];
+
+function availableCurr(arr, missingCurr) {
+
+}
